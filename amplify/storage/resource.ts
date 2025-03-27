cbb8441128ff97a61fee29b63d14cd2e3dd83fbc
Yudiz-Harshil <harshil.thummar@yudizsolutions.com>
2025-03-27 18:17:27 +0530

    Permission_Test

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -7,12 +7,12 @@ export const storage = defineStorage({
       allow.guest.to(['read']),
       allow.authenticated.to(['read']),
     ],
-    'protected/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
+    //'protected/{entity_id}/*': [
+    //  allow.authenticated.to(['read']),
+    //  allow.entity('identity').to(['read', 'write', 'delete'])
+    //],
+    //'private/{entity_id}/*': [
+    //  allow.entity('identity').to(['read', 'write', 'delete'])
+    //]
   })
 });
