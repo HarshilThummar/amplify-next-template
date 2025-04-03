86df64370c64d74e19d5b4b0d0ed89b84c6ab463
Yudiz-Harshil <harshil.thummar@yudizsolutions.com>
2025-04-03 16:36:37 +0530

    Auth change

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,21 @@
-import { defineAuth } from "@aws-amplify/backend";
+// import { defineAuth } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
+// /**
+//  * Define and configure your auth resource
+//  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+//  */
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//   },
+// });
+
+import { referenceAuth } from '@aws-amplify/backend';
+
+export const auth = referenceAuth({
+  userPoolId: process.env.REACT_APP_MY_USER_POOL_ID as string,
+  identityPoolId: process.env.REACT_APP_MY_IDENTITY_POOL_ID as string,
+  authRoleArn: process.env.REACT_APP_MY_AUTH_ROLE_ARN as string,
+  unauthRoleArn: process.env.REACT_APP_MY_UNAUTH_ROLE_ARN as string,
+  userPoolClientId: process.env.REACT_APP_MY_USER_POOL_CLIENT_ID as string,
 });
